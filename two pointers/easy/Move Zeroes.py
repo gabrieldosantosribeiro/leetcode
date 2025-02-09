064cc5a1f0b06e1f3078dba85296ef102037c47e
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-02-08 23:20:34 -0300

    Create Move Zeroes.py

---
+++ two pointers/easy/Move Zeroes.py
@@ -0,0 +1,42 @@
+'''
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+'''
+
+# solution
+def moveZeroes(self, nums):
+
+    left = 0
+    right = len(nums)
+
+    while left < right:
+        if nums[left] != 0:
+            left += 1
+        else:
+            nums.pop(left)
+            nums.append(0)
+            right -= 1
+            
+    return nums
+    
